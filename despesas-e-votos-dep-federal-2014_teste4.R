ccb1e20bf8d5cb83cbe9a800da5c6c3d690f626c
gabrielacaesar <gabrielacaesar55@gmail.com>
2018-11-05 05:09:48 -0200

    Update despesas-e-votos-dep-federal-2014_teste4.R

--- despesas-e-votos-dep-federal-2014_teste4.R
+++ despesas-e-votos-dep-federal-2014_teste4.R
@@ -22,7 +22,8 @@ gc_scatterplot4 <- ggplot(dep_federal_2014, aes(x = dep_federal_2014$procv_despe
                                                 col = "red")) +
   geom_point(position = "jitter", alpha = 0.5) +
   labs(y = "UF", caption = "Fonte: TSE e Camara dos Deputados") +
-  scale_x_continuous(limits = c(100000,9000000)) +
+  scale_x_continuous(limits = c(100000,9000000), 
+                     labels = scales::comma) +
   ggtitle("Eleicao 2014: Despesas totais (em RS) dos deputados federais eleitos") +
   theme(legend.position="none",
         axis.title.y = element_blank(),
