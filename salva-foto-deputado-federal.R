b5c7e45bdab8afae79ab8bf27c1b902d5dfa9adf
gabrielacaesar <gabrielacaesar55@gmail.com>
2019-01-16 19:12:43 -0200

    Update salva-foto-deputado-federal.R

--- salva-foto-deputado-federal.R
+++ salva-foto-deputado-federal.R
@@ -1,10 +1,13 @@
 # passo a passo
 # importar um df com os links da API com as infos de cada deputado
-# criar uma coluna 'name-lower', com o nome de cada deputado em lower, sem acento, separado com traço
+# criar uma coluna 'name-lower', com o nome de cada deputado em lower, sem acento, separado com traÃ§o
 # acrescentar a coluna 'name-lower' ao df antigo
 # acessar o xml e pegar a url dentro de 'urlFoto'
-# salvar foto com o nome contido dentro da coluna 'name-lower'
+# salvar foto com o id de cada deputado
 # repetir procedimento para cada um dos 513 deputados eleitos
+
+# trocar o id de cada deputado por 'name-lower'
+
 
 # etapa 1
 ## carregando as bibliotecas
@@ -13,7 +16,7 @@ install.packages("stringr")
 install.packages("XML")
 install.packages("xml2")
 install.packages("methods")
-install.packages(c("httr", "jsonlite", "lubridate"))
+install.packages("httr")
 library(dplyr)
 library(readr)
 library(readxl)
@@ -26,6 +29,7 @@ library(methods)
 library(httr)
 library(jsonlite)
 library(lubridate)
+library(httr)
 
 getwd()
 setwd("~/Downloads")
@@ -33,7 +37,7 @@ setwd("~/Downloads")
 # importamos o csv
 deputados <- fread("dep-legislatura56-14jan2019.csv")
 
-# tiramos a acentuação
+# tiramos a acentuaÃ§Ã£o
 deputados_sem_acentuacao <- as.data.frame(abjutils::rm_accent(deputados$nome))
 colnames(deputados_sem_acentuacao) <- "nome_deputado"
 
@@ -41,14 +45,19 @@ colnames(deputados_sem_acentuacao) <- "nome_deputado"
 deputados_sem_acentuacao <- as.data.frame(tolower(deputados_sem_acentuacao$nome_deputado))
 colnames(deputados_sem_acentuacao) <- "nome_deputado"
 
-# substituímos espaço por hífen
+# substituÃ­mos espaÃ§o por hÃ­fen
 deputados_sem_acentuacao <- as.data.frame(sub(" ", "-", deputados_sem_acentuacao$nome_deputado))
 colnames(deputados_sem_acentuacao) <- "name_lower"
 
 # acrescentamos nova coluna ao antigo dataframe
 deputados[,'name_lower'] <- deputados_sem_acentuacao
 
-# deletamos colunas desnecessárias
+### em andamento
+# criamos uma coluna apenas com o id de cada deputado
+deputados_id <- strsplit(deputados$uri, "/deputados/")
+deputados_id <- deputados_id[2]
+
+# deletamos colunas desnecessÃ¡rias
 deputados$idLegislaturaInicial <- NULL
 deputados$idLegislaturaFinal <- NULL
 deputados$cpf <- NULL
@@ -61,5 +70,13 @@ deputados$municipioNascimento <- NULL
 
 # acessar o xml e pegar a url dentro de 'urlFoto'
 
-url <- "https://dadosabertos.camara.leg.br/api/v2/deputados/66179"
 
+while(i <= 514) {
+  url <- deputados$uri[i]
+  api_content <- rawToChar(GET(url)$content)
+  pessoa_info <- jsonlite::fromJSON(api_content)
+  pessoa_foto <- pessoa_info$dados$ultimoStatus$urlFoto
+  download.file(pessoa_foto, basename(pessoa_foto), mode = "wb")
+  Sys.sleep(0.3)
+  i <- i + 1
+}
