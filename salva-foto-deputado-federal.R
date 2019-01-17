846f13ffc07eb300b482db9654951685f6b67170
gabrielacaesar <gabrielacaesar55@gmail.com>
2019-01-17 20:01:57 -0200

    Update salva-foto-deputado-federal.R

--- salva-foto-deputado-federal.R
+++ salva-foto-deputado-federal.R
@@ -2,6 +2,7 @@
 # importar um df com os links da API com as infos de cada deputado
 # criar uma coluna 'name-lower', com o nome de cada deputado em lower, sem acento, separado com traço
 # acrescentar a coluna 'name-lower' ao df antigo
+
 # acessar o xml e pegar a url dentro de 'urlFoto'
 # salvar foto com o id de cada deputado
 # repetir procedimento para cada um dos 513 deputados eleitos
@@ -54,10 +55,6 @@ colnames(deputados_sem_acentuacao) <- "name_lower"
 # acrescentamos nova coluna ao antigo dataframe
 deputados[,'name_lower'] <- deputados_sem_acentuacao
 
-### em andamento
-# criamos uma coluna apenas com o id de cada deputado
-deputados_id <- strsplit(deputados$uri, "/deputados/")
-deputados_id <- deputados_id[2]
 
 # deletamos colunas desnecessárias
 deputados$idLegislaturaInicial <- NULL
@@ -70,21 +67,54 @@ deputados$dataNascimento <- NULL
 deputados$dataFalecimento <- NULL
 deputados$municipioNascimento <- NULL
 
-# acessar o xml e pegar a url dentro de 'urlFoto'
 
-setwd("~/Downloads/deputados2019")
+# criando um novo diretório em downloads
+# entrando neste novo diretório
+setwd("~/Downloads/")
+dir.create("deputados-new")
+setwd("~/Downloads/deputados-new")
 
+
+# loop para baixar todos os arquivos de fotos
+# acessa o xml e pega a url dentro de 'urlFoto'
+# e ignora links que não funcionam
 i <- 1
-
 while(i <= 514) {
   tryCatch({
     url <- deputados$uri[i]
     api_content <- rawToChar(GET(url)$content)
     pessoa_info <- jsonlite::fromJSON(api_content)
     pessoa_foto <- pessoa_info$dados$ultimoStatus$urlFoto
-    download.file(pessoa_foto, basename(pessoa_foto), mode = "wb")
+    download.file(pessoa_foto, basename(pessoa_foto), mode = "wb", header = TRUE)
     Sys.sleep(0.5)
   }, error = function(e) return(NULL)
   )
   i <- i + 1
 }
+
+
+photos <- list.files(
+  recursive = TRUE,
+  full.names = TRUE
+)
+
+
+# loop para renomear as fotos
+# em vez de id, queremos 'name-lower'
+for (p in photos) {
+  id <- basename(p)
+  id <- gsub(".jpg$", "", id)
+  name <- deputados$name_lower[match(id, basename(deputados$uri))]
+  fname <- paste0(dirname(p), "/", name, ".jpg")
+  file.rename(p, fname)
+  
+  #optional
+  cat("renaming", basename(p), "to", name, "\n")
+}
+
+
+### em andamento
+# falta descobrir quais deputados tiveram erro na foto
+# temos 271 fotos neste momento
+# há fotos que foram salvas antes de o download estar 100% (rosangela gomes, por exemplo)?
+# sobre fotos ausentes: caso tecnologia da CD não responde, entrar em contato com a ascom
