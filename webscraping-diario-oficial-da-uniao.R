34fb342927f93ab35d83bbf73b9d6c0b397bdf19
gabrielacaesar <gabrielacaesar55@gmail.com>
2019-08-02 02:20:36 -0300

    Update webscraping-diario-oficial-da-uniao.R

--- webscraping-diario-oficial-da-uniao.R
+++ webscraping-diario-oficial-da-uniao.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(rvest)
 library(xml2)
 library(janitor)
+library(zoo)
 
 # coleta de links de portarias
 
@@ -31,7 +32,55 @@ conteudo <- read_html(portaria) %>%
   str_trim() %>%
   as.data.frame(stringsAsFactors = FALSE) %>%
   `colnames<-`("conteudo") %>%
-  filter(!conteudo == "") %>%
+  filter(!conteudo == "",
+         !str_detect(conteudo, 
+        "A pessoa referida nesta Portaria"),
+        !str_detect(conteudo, 
+        "As pessoas referidas nesta Portaria"),
+         !str_detect(conteudo, 
+        "A SECRETÁRIA NACIONAL DE JUSTIÇA, DO MINISTÉRIO DA JUSTIÇA E SEGURANÇA PÚBLICA")) %>%
   mutate(data = data_conteudo) %>%
   mutate(n_portaria = ifelse(str_detect(conteudo, "Nº"), 
-                             conteudo, "NA")
+                             conteudo, NA)) %>%
+  mutate(n_portaria = na.locf(n_portaria)) %>%
+  filter(!str_detect(conteudo, "Nº "))
+  
+# limpeza do conteudo
+# separando em colunas
+conteudo_tidy <- conteudo %>%
+  separate(conteudo, c("nome", "restante"), sep = " - ") %>%
+  separate(restante, c("processo", "restante"), sep = ", natural") %>%
+  mutate(restante = str_replace_all(restante, "nascida", "nascido")) %>%
+  separate(restante, c("pais", "restante"), sep = ", nascido em ") %>%
+  mutate(restante = str_replace_all(restante, "filha", "filho")) %>%
+  separate(restante, c("data_nascimento", "restante"), sep = ", filho de") %>%
+  separate(restante, c("genitores", "restante"), sep = ", residente ") %>%
+  separate(restante, c("uf", "processo"), sep = " \\(Processo ") %>%
+  separate(n_portaria, c("num_portaria", "texto_portaria"), sep = " - ")
+
+conteudo_tidy_2 <- conteudo_tidy %>%
+  mutate(pais = str_remove_all(pais, "de "),
+         pais = str_remove_all(pais, "do "),
+         pais = str_remove_all(pais, "da "),
+         uf = str_remove_all(uf, "no Estado de "),
+         uf = str_remove_all(uf, "no Estado do "),
+         uf = str_remove_all(uf, "no Estado da "),
+         num_portaria = str_remove_all(num_portaria, "Nº"),
+         processo = str_remove_all(processo, "\\) e"),
+         processo = str_remove_all(processo, "\\)."),
+         processo = str_remove_all(processo, "\\);")) %>%
+  mutate(genitores = str_replace_all(genitores, " e de ", "; "),
+         genitores = str_replace_all(genitores, " e ", "; "),
+         data_nascimento = str_replace_all(data_nascimento, " de janeiro de ", "/01/"),
+         data_nascimento = str_replace_all(data_nascimento, " de fevereiro de ", "/02/"),
+         data_nascimento = str_replace_all(data_nascimento, " de março de ", "/03/"),
+         data_nascimento = str_replace_all(data_nascimento, " de abril de ", "/04/"),
+         data_nascimento = str_replace_all(data_nascimento, " de maio de ", "/05/"),
+         data_nascimento = str_replace_all(data_nascimento, " de junho de ", "/06/"),
+         data_nascimento = str_replace_all(data_nascimento, " de julho de ", "/07/"),
+         data_nascimento = str_replace_all(data_nascimento, " de agosto de ", "/08/"),
+         data_nascimento = str_replace_all(data_nascimento, " de setembro de ", "/09/"),
+         data_nascimento = str_replace_all(data_nascimento, " de outubro de ", "/10/"),
+         data_nascimento = str_replace_all(data_nascimento, " de novembro de ", "/11/"),
+         data_nascimento = str_replace_all(data_nascimento, " de dezembro de ", "/12/"))
+  
