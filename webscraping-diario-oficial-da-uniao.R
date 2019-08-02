909d2352d523015de426b7161b6a06e634138b81
gabrielacaesar <gabrielacaesar55@gmail.com>
2019-08-02 03:31:30 -0300

    Update webscraping-diario-oficial-da-uniao.R

--- webscraping-diario-oficial-da-uniao.R
+++ webscraping-diario-oficial-da-uniao.R
@@ -6,7 +6,7 @@ library(zoo)
 
 # coleta de links de portarias
 
-url <- "http://www.in.gov.br/consulta?q=%22CONCEDER%20a%20nacionalidade%20brasileira%22&publishFrom=2019-01-01&publishTo=2019-07-31"
+url <- "http://www.in.gov.br/consulta?q=%22CONCEDER%20a%20nacionalidade%20brasileira%22&publishFrom=2019-01-01&publishTo=2019-07-31&start=1"
 
 urls_portaria <- url %>%
   read_html() %>%
@@ -15,39 +15,54 @@ urls_portaria <- url %>%
   as.data.frame() %>%
   `colnames<-`("links") %>%
   filter(str_detect(links, "http://www.in.gov.br/web/dou/")) %>%
-  ungroup()
+  ungroup() %>%
+  filter(!str_detect(links, "\\?inheritRedirect=true"))
+
 
 # coleta de conteúdo das portarias
 
-portaria <- as.character(urls_portaria$links[1])
+conteudo <- NULL
+i <- 1
 
-data_conteudo <- read_html(portaria) %>%
-  html_nodes("p.identifica") %>%
-  html_text() %>%
-  str_trim()
-
-conteudo <- read_html(portaria) %>%
-  html_nodes("p.dou-paragraph") %>%
-  html_text() %>%
-  str_trim() %>%
-  as.data.frame(stringsAsFactors = FALSE) %>%
-  `colnames<-`("conteudo") %>%
-  filter(!conteudo == "",
-         !str_detect(conteudo, 
-        "A pessoa referida nesta Portaria"),
-        !str_detect(conteudo, 
-        "As pessoas referidas nesta Portaria"),
-         !str_detect(conteudo, 
-        "A SECRETÁRIA NACIONAL DE JUSTIÇA, DO MINISTÉRIO DA JUSTIÇA E SEGURANÇA PÚBLICA")) %>%
-  mutate(data = data_conteudo) %>%
-  mutate(n_portaria = ifelse(str_detect(conteudo, "Nº"), 
-                             conteudo, NA)) %>%
-  mutate(n_portaria = na.locf(n_portaria)) %>%
-  filter(!str_detect(conteudo, "Nº "))
+while(i < 20) {
+  tryCatch({
+  portaria <- as.character(urls_portaria$links[i])
+  data_conteudo <- read_html(portaria) %>%
+    html_nodes("p.identifica") %>%
+    html_text() %>%
+    str_trim()
   
-# limpeza do conteudo
+  conteudo <- read_html(portaria) %>%
+    html_nodes("p.dou-paragraph") %>%
+    html_text() %>%
+    str_trim() %>%
+    as.data.frame(stringsAsFactors = FALSE) %>%
+    `colnames<-`("conteudo") %>%
+    filter(!conteudo == "",
+           !str_detect(conteudo, 
+          "A pessoa referida nesta Portaria"),
+          !str_detect(conteudo, 
+          "As pessoas referidas nesta Portaria"),
+           !str_detect(conteudo, 
+          "A SECRETÁRIA NACIONAL DE JUSTIÇA, DO MINISTÉRIO DA JUSTIÇA E SEGURANÇA PÚBLICA")) %>%
+    mutate(data = data_conteudo) %>%
+    mutate(n_portaria = ifelse(str_detect(conteudo, "Nº"), 
+                               conteudo, NA)) %>%
+    mutate(n_portaria = na.locf(n_portaria)) %>%
+    filter(!str_detect(conteudo, "Nº")) %>%
+    rbind(conteudo)
+  }, error = function(e) return(NULL)
+  )
+  i <- i + 1
+}
+
+
+
+# limpeza do conteudo - 1
 # separando em colunas
 conteudo_tidy <- conteudo %>%
+  mutate(conteudo = str_replace_all(conteudo, "CESAR AUGUSTO SANCHEZ ALARCON, ", 
+                                    "CESAR AUGUSTO SANCHEZ ALARCON - sem numero de processo, ")) %>%
   separate(conteudo, c("nome", "restante"), sep = " - ") %>%
   separate(restante, c("processo", "restante"), sep = ", natural") %>%
   mutate(restante = str_replace_all(restante, "nascida", "nascido")) %>%
@@ -55,13 +70,22 @@ conteudo_tidy <- conteudo %>%
   mutate(restante = str_replace_all(restante, "filha", "filho")) %>%
   separate(restante, c("data_nascimento", "restante"), sep = ", filho de") %>%
   separate(restante, c("genitores", "restante"), sep = ", residente ") %>%
-  separate(restante, c("uf", "processo"), sep = " \\(Processo ") %>%
+  mutate(restante = str_replace_all(restante, "do Pará", "do Pará "),
+        restante = str_replace_all(restante, " Processo", "Processo")) %>%
+  separate(restante, c("uf", "processo"), sep = "\\(Processo ") %>%
+  mutate(n_portaria = str_replace_all(n_portaria, "96 CONCEDER", "96 - CONCEDER"),
+         n_portaria = str_replace_all(n_portaria, "95 CONCEDER", "95 - CONCEDER")) %>%
   separate(n_portaria, c("num_portaria", "texto_portaria"), sep = " - ")
 
+# limpeza do conteudo - 2
+# fazendo ajustes e corrigindo erros
 conteudo_tidy_2 <- conteudo_tidy %>%
   mutate(pais = str_remove_all(pais, "de "),
          pais = str_remove_all(pais, "do "),
          pais = str_remove_all(pais, "da "),
+         pais = str_remove_all(pais, "dos "),
+        pais = str_trim(pais),
+        pais = str_replace_all(pais, "Guiné Bissau", "Guiné-Bissau"),
          uf = str_remove_all(uf, "no Estado de "),
          uf = str_remove_all(uf, "no Estado do "),
          uf = str_remove_all(uf, "no Estado da "),
@@ -71,6 +95,7 @@ conteudo_tidy_2 <- conteudo_tidy %>%
          processo = str_remove_all(processo, "\\);")) %>%
   mutate(genitores = str_replace_all(genitores, " e de ", "; "),
          genitores = str_replace_all(genitores, " e ", "; "),
+         pais = str_replace_all(pais, "EmiraÁrabes", "Emirados Árabes"),
          data_nascimento = str_replace_all(data_nascimento, " de janeiro de ", "/01/"),
          data_nascimento = str_replace_all(data_nascimento, " de fevereiro de ", "/02/"),
          data_nascimento = str_replace_all(data_nascimento, " de março de ", "/03/"),
@@ -83,4 +108,7 @@ conteudo_tidy_2 <- conteudo_tidy %>%
          data_nascimento = str_replace_all(data_nascimento, " de outubro de ", "/10/"),
          data_nascimento = str_replace_all(data_nascimento, " de novembro de ", "/11/"),
          data_nascimento = str_replace_all(data_nascimento, " de dezembro de ", "/12/"))
+  
+  
+  
   
