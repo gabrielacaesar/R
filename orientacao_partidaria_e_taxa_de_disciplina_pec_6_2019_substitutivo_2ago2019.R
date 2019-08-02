e99f1771f6c929c8dee8abc406e42830d9d9b124
gabrielacaesar <gabrielacaesar55@gmail.com>
2019-08-02 19:48:22 -0300

    Update orientacao_partidaria_e_taxa_de_disciplina_pec_6_2019_substitutivo_2ago2019.R

--- orientacao_partidaria_e_taxa_de_disciplina_pec_6_2019_substitutivo_2ago2019.R
+++ orientacao_partidaria_e_taxa_de_disciplina_pec_6_2019_substitutivo_2ago2019.R
@@ -1,104 +1,142 @@
 # carregar as bibliotecas
+
 library(tidyverse)
 library(foreign)
 library(data.table)
+library(DescTools)
 
-# definir diretório
-setwd("~/Downloads/votacoes-nominais")
 
-orientacao_partidos <- fread("orientacao_partidos.csv", encoding = "UTF-8", header = T)
-votos_deputados <- fread("votos_deputados.csv", encoding = "UTF-8", header = T)
+# inserir link de votaÃ§Ã£o
 
-# delete index column
-orientacao_partidos$V1 <- NULL
-votos_deputados$V1 <- NULL
+url <- "https://www.camara.leg.br/internet/votacao/mostraVotacao.asp?ideVotacao=8974&numLegislatura=56&codCasa=1&numSessaoLegislativa=1&indTipoSessaoLegislativa=O&numSessao=191&indTipoSessao=E&tipo=partido"
+
+
+################################################################
+###                  voto de cada deputado                   ###
+################################################################
+
+# pegar tabela de voto
+
+table_voto <- read_html(url) %>%
+  html_nodes("table") %>%
+  html_table(fill = TRUE, header = T) %>%
+  .[[3]]
+
+table1_df <- data.frame(table_voto) %>%
+  `colnames<-`(c("nome", "uf", "voto")) %>%
+  mutate(new_column = NA)
+
+# corrigir falha da formataÃ§Ã£o da tabela
+
+idx <- grep("Total.*: \\d+", table1_df$nome)
+
+for (i in seq_along(idx)){
+  n <- as.numeric(sub("^.*: ", "", table1_df$nome[idx[i]]))
+  partido <- sub("Total ", "", table1_df$nome[idx[i]])
+  partido <- sub(": .*", "", partido)
+  table1_df$new_column[(idx[i] - n):(idx[i] - 1)] <- partido
+}
+
+table1_df <- table1_df[-grep("Total .*:.*", table1_df$nome), ]
+table1_df <- table1_df[-which(table1_df$nome == table1_df$uf), ]
+colnames(table1_df)[4] <- "partido"
+
+# padronizar votos
+
+table1_df$voto[table1_df$voto == "Sim"] <- "sim"
+table1_df$voto[table1_df$voto == "NÃ£o"] <- "nao"
+table1_df$voto[table1_df$voto == "AbstenÃ§Ã£o"] <- "abstencao"
+table1_df$voto[table1_df$voto == "ObstruÃ§Ã£o"] <- "obstrucao"
+table1_df$voto[table1_df$voto == "Art. 17"] <- "naovotou"
 
 # padronizar partidos
-votos_deputados$partido[votos_deputados$partido == "Podemos"] <- "PODE"
-votos_deputados$partido[votos_deputados$partido == "REDE"] <- "Rede"
-votos_deputados$partido[votos_deputados$partido == "Solidaried"] <- "SD"
-votos_deputados$partido[votos_deputados$partido == "NOVO"] <- "Novo"
-votos_deputados$partido[votos_deputados$partido == "S.Part."] <- "S/Partido"
-votos_deputados$partido[votos_deputados$partido == "PPS"] <- "Cidadania"
-votos_deputados$partido[votos_deputados$partido == "CIDADANIA"] <- "Cidadania"
-votos_deputados$partido[votos_deputados$partido == "PR"] <- "PL"
+
+table1_df$partido <- as.character(table1_df$partido)
+table1_df$partido[table1_df$partido == "Podemos"] <- "PODE"
+table1_df$partido[table1_df$partido == "REDE"] <- "Rede"
+table1_df$partido[table1_df$partido == "Solidaried"] <- "SD"
+table1_df$partido[table1_df$partido == "NOVO"] <- "Novo"
+table1_df$partido[table1_df$partido == "S.Part."] <- "S/Partido"
+table1_df$partido[table1_df$partido == "PPS"] <- "Cidadania"
+table1_df$partido[table1_df$partido == "CIDADANIA"] <- "Cidadania"
+table1_df$partido[table1_df$partido == "PR"] <- "PL"
 
 # padronizar nome que vem com erros
-votos_deputados$nome[votos_deputados$nome == "Chico D`Angelo"] <- "Chico D'Angelo"
-votos_deputados$nome[votos_deputados$nome == "Flávio Nogueira"] <- "Flavio Nogueira"
-votos_deputados$nome[votos_deputados$nome == "Jhc"] <- "JHC"
 
-# agrupar 
-votos_deputados_n <- votos_deputados %>%
-  group_by(nome, partido, voto, id) %>%
-  summarise() 
+table1_df$nome[table1_df$nome == "Chico D`Angelo"] <- "Chico D'Angelo"
+table1_df$nome[table1_df$nome == "FlÃ¡vio Nogueira"] <- "Flavio Nogueira"
+table1_df$nome[table1_df$nome == "Jhc"] <- "JHC"
 
-# corrigindo os dados de orientação por partido
-orientacao_partidos_n <- orientacao_partidos %>%
+
+
+################################################################
+###               orientaÃ§Ã£o de partidos e blocos            ###
+################################################################
+
+table_orientacao <- read_html(url) %>%
+  html_nodes("table") %>%
+  html_table(fill = TRUE, header = F) %>%
+  .[[2]] %>%
+  `colnames<-`(c("partido", "orientacao"))
+
+orientacao_partidos_n <- table_orientacao %>%
   distinct() %>%
-  mutate(partido = str_replace_all(partido, ":", "")) %>%
-  mutate(partido = str_replace_all(partido, "Repr.", "")) %>%
-  mutate(partido = str_replace_all(partido, "PslPpPsd...", "PSL - PP - PSD")) %>%
-  mutate(partido = str_replace_all(partido, "PdtSdPode...", "PDT - SD - PODE")) %>%
-  mutate(partido = str_replace_all(partido, "PdtPodePros...", "PDT - PODE - PROS")) %>%
-  mutate(partido = str_replace_all(partido, "PdtPros...", "PDT - PROS")) %>%
-  mutate(partido = str_replace_all(partido, "PpPrPsd", "PP - PL - PSD")) %>%
-  mutate(partido = str_replace_all(partido, "PpMdbPtb", "PP - MDB - PTB")) %>%
-  mutate(partido = str_replace_all(partido, "PdtAvante", "PDT - Avante")) %>%
-  mutate(partido = str_replace_all(partido, "GOV.", "Governo")) %>%
-  mutate(partido = str_replace_all(partido, "CIDADANIA", "Cidadania")) %>%
-  mutate(partido = str_replace_all(partido, "NOVO", "Novo")) %>%
-  mutate(partido = str_replace_all(partido, "Solidaried", "SD")) %>%
-  mutate(partido = str_replace_all(partido, "Podemos", "PODE")) %>%
-  mutate(partido = str_replace_all(partido, "REDE", "Rede")) %>%
-  arrange(id)
+  mutate(partido = str_replace_all(partido, ":", ""),
+        partido = str_replace_all(partido, "Repr.", ""),
+        partido = str_replace_all(partido, "PpMdbPtb", "PP - MDB - PTB"),
+        partido = str_replace_all(partido, "PslPpPsd...", "PSL - PP - PSD"),
+        partido = str_replace_all(partido, "PdtSdPode...", "PDT - SD - PODE"),
+        partido = str_replace_all(partido, "PdtPodePros...", "PDT - PODE - PROS"),
+        partido = str_replace_all(partido, "PdtPros...", "PDT - PROS"),
+        partido = str_replace_all(partido, "PpPrPsd", "PP - PL - PSD"),
+        partido = str_replace_all(partido, "PpMdbPtb", "PP - MDB - PTB"),
+        partido = str_replace_all(partido, "PdtAvante", "PDT - Avante"),
+        partido = str_replace_all(partido, "GOV.", "Governo"),
+        partido = str_replace_all(partido, "CIDADANIA", "Cidadania"),
+        partido = str_replace_all(partido, "NOVO", "Novo"),
+        partido = str_replace_all(partido, "Solidaried", "SD"),
+        partido = str_replace_all(partido, "Podemos", "PODE"),
+        partido = str_replace_all(partido, "REDE", "Rede")) %>%
+  mutate(orientacao = str_replace_all(orientacao, "Sim", "sim"),
+        orientacao = str_replace_all(orientacao, "NÃ£o", "nao"),
+        orientacao = str_replace_all(orientacao, "Liberado", "liberado")) %>%
+  arrange(partido)
 
-# selecionando apenas linhas que precisam de alteração
 orientacao_partidos_tidy <- orientacao_partidos_n  %>%
-  filter(partido %like% " - ") %>%
+  filter(str_detect(partido, " - ")) %>%
   separate(partido, c("partido1", "partido2", "partido3"), " - ")
 
-# separando por colunas e considerando partido1
 partido1 <- orientacao_partidos_tidy %>%
-  select(partido1, orientacao, id) %>%
-  `colnames<-`(c("partido", "orientacao", "id"))
+  select(partido1, orientacao) %>%
+  `colnames<-`(c("partido", "orientacao"))
 
-# separando por colunas e considerando partido2
 partido2 <- orientacao_partidos_tidy %>%
-  select(partido2, orientacao, id) %>%
-  `colnames<-`(c("partido", "orientacao", "id"))
+  select(partido2, orientacao) %>%
+  `colnames<-`(c("partido", "orientacao"))
 
-# separando por colunas e considerando partido3
 partido3 <- orientacao_partidos_tidy %>%
-  select(partido3, orientacao, id) %>%
-  `colnames<-`(c("partido", "orientacao", "id"))
+  select(partido3, orientacao) %>%
+  `colnames<-`(c("partido", "orientacao"))
 
-# criando df com partidos corrigidos
-partido_new <- rbind(partido1, partido2, partido3) %>%
-  arrange(id)
+partido_new <- rbind(partido1, partido2, partido3)
 
-# unindo do df original com os novos partidos
 df_final <- orientacao_partidos_n %>%
-  filter(!partido %like% " - ") %>%
+  filter(!str_detect(partido, " - ")) %>%
   rbind(partido_new) %>%
-  arrange(id)
+  arrange(partido)
 
-###### 
-# dar merge considerando partido e id
-# df com voto e df com orientação
-######
+################################################################
+###               joining and comparing columns             ###
+################################################################
 
-joined_data <- votos_deputados_n %>%
-  left_join(df_final, by.x = c("partido", "id"),
-            by.y = c("partido", "id")) %>%
-  mutate(orientacao = replace_na(orientacao, "nao-orientou")) %>%
-  filter(id == "CD190208") # código se refere à votação do texto-base
-# ACIMA, caso opte por outra votação, apenas trocar o código da votação
+joined_data <- table1_df %>%
+  left_join(df_final, by = "partido") %>%
+  mutate(orientacao = replace_na(orientacao, "nao-orientou")) 
 
-# comparar colunas voto e orientacao
+# por partido
 
 joined_data_2 <- joined_data %>%
-  filter(orientacao != "Liberado" & orientacao != "nao-orientou") %>%
+  filter(orientacao != "liberado" & orientacao != "nao-orientou") %>%
   mutate(check = ifelse(voto == orientacao, "match", "not_match")) %>%
   group_by(partido, check) %>%
   summarise(int = n()) %>%
@@ -109,6 +147,18 @@ joined_data_2 <- joined_data %>%
   mutate(match_perc = (match / total) * 100) %>%
   mutate(not_match_perc = (not_match / total) * 100) %>%
   arrange(desc(not_match_perc))
-  
 
-write.csv(joined_data_2, "partido_orientacao_voto_pec_6_2019_substitutivo.csv")
+View(joined_data_2)
+
+write.csv(joined_data_2, "joined_data_2.csv")
+
+# por nome
+
+joined_data_3 <- joined_data %>%
+  mutate(check = ifelse(voto == orientacao, "match", "not_match")) %>%
+  arrange(desc(check))
+
+View(joined_data_3)
+
+write.csv(joined_data_3, "joined_data_3.csv")
+
