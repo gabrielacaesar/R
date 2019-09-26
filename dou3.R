86ef7f21430a2361769e84175a704d300ce5c196
gabrielacaesar <gabrielacaesar55@gmail.com>
2019-09-26 19:50:07 -0300

    Update dou3.R

--- dou3.R
+++ dou3.R
@@ -21,33 +21,77 @@ i <- 1
 
 while(i < 55) {
   tryCatch({
-url_portaria <- as.character(conteudo_urls$urls[i])
-
-extracao_portaria <- read_html(url_portaria) %>%
-  html_nodes("p.dou-paragraph") %>%
-  html_text() %>%
-  str_trim() %>%
-  as.data.frame(stringsAsFactors = FALSE) %>%
-  `colnames<-`("txt_portaria") %>%
-  mutate(txt_portaria = toupper(txt_portaria)) %>%
-  filter(txt_portaria != "") %>%
-  mutate(conteudo_portaria = ifelse(str_detect(txt_portaria, "DECLARAR A PERDA"), 
-                                    txt_portaria, NA)) %>%
-  fill(conteudo_portaria, .direction = "down") %>%
-  filter(!str_detect(txt_portaria, "DECLARAR A PERDA"),
-         !str_detect(txt_portaria, "COORDENADOR DE PROCESSOS"),
-         !str_detect(txt_portaria, "SECRETÁRIA NACIONAL"),
-         !str_detect(txt_portaria, "MINISTRO DE ESTADO"),
-         !str_detect(txt_portaria, "SECRETÁRIO NACIONAL"),
-         !str_detect(txt_portaria, "EXPULSAR")) %>%
-  mutate(url_portaria = url_portaria) %>%
-  rbind(extracao_portaria)
+    url_portaria <- as.character(conteudo_urls$urls[i])
+    
+    extracao_portaria <- read_html(url_portaria) %>%
+      html_nodes("p.dou-paragraph") %>%
+      html_text() %>%
+      str_trim() %>%
+      as.data.frame(stringsAsFactors = FALSE) %>%
+      `colnames<-`("txt_portaria") %>%
+      mutate(txt_portaria = toupper(txt_portaria)) %>%
+      filter(txt_portaria != "") %>%
+      mutate(conteudo_portaria = ifelse(str_detect(txt_portaria, "DECLARAR A PERDA"), 
+                                        txt_portaria, NA)) %>%
+      fill(conteudo_portaria, .direction = "down") %>%
+      filter(!str_detect(txt_portaria, "DECLARAR A PERDA"),
+             !str_detect(txt_portaria, "COORDENADOR DE PROCESSOS"),
+             !str_detect(txt_portaria, "SECRETÁRIA NACIONAL"),
+             !str_detect(txt_portaria, "MINISTRO DE ESTADO"),
+             !str_detect(txt_portaria, "SECRETÁRIO NACIONAL"),
+             !str_detect(txt_portaria, "EXPULSAR"),
+             !str_detect(txt_portaria, "AS PESSOAS REFERIDAS"),
+             !str_detect(txt_portaria, "A PESSOA REFERIDA"),
+             !str_detect(txt_portaria, "A COORDENADORA DE PROCESSOS"),
+             !str_detect(txt_portaria, "DEPARTAMENTO PENITENCIÁRIO"),
+             !str_detect(txt_portaria, "CONCEDER A NACIONALIDADE")) %>%
+      mutate(url_portaria = url_portaria) %>%
+      rbind(extracao_portaria)
   }, error = function(e) return(NULL)
   )
   i <- i + 1
 }
 
-extracao_portaria <- NULL
 
 # acima temos 44 de 51 urls
-# impedir inclusao de concessoes
+# abaixo teremos o conteúdo das 7 urls restantes
+# URLs que não foram incorporadas por HTML diferente
+i <- 1
+
+while(i < 55) {
+  tryCatch({
+  url_portaria <- as.character(conteudo_urls$urls[i])
+  
+    extracao_portaria_2 <- read_html(url_portaria) %>%
+      html_nodes("p.corpo.pdf-JUSTIFY") %>%
+      html_text() %>%
+      str_trim() %>%
+      as.data.frame(stringsAsFactors = FALSE) %>%
+      `colnames<-`("txt_portaria") %>%
+      mutate(txt_portaria = toupper(txt_portaria)) %>%
+      mutate(conteudo_portaria = ifelse(str_detect(txt_portaria, "DECLARAR A PERDA"), 
+                                        txt_portaria, NA)) %>%
+      fill(conteudo_portaria, .direction = "down") %>%
+      filter(!str_detect(txt_portaria, "DECLARAR A PERDA"),
+             !str_detect(txt_portaria, "COORDENADOR DE PROCESSOS"),
+             !str_detect(txt_portaria, "SECRETÁRIA NACIONAL"),
+             !str_detect(txt_portaria, "MINISTRO DE ESTADO"),
+             !str_detect(txt_portaria, "SECRETÁRIO NACIONAL"),
+             !str_detect(txt_portaria, "EXPULSAR")) %>%
+      mutate(url_portaria = url_portaria) %>%
+      rbind(extracao_portaria_2)
+    }, error = function(e) return(NULL)
+    )
+i <- i + 1
+}
+
+# juntar arquivos 
+# remover duplicatas
+
+new <- rbind(extracao_portaria, extracao_portaria_2)
+
+teste <- new %>%
+  distinct()
+
+# conferir quais links têm concessao
+# eliminar sujeitas e linhas excedentes
