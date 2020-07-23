94e9472d9e6c02d8b4e25f108851f64cc84df2e3
gabrielacaesar <gabrielacaesar55@gmail.com>
2020-07-23 13:38:49 -0300

    Update o-voto-dos-senadores-virtual.R

--- ovotodascasas/o-voto-dos-senadores-virtual.R
+++ ovotodascasas/o-voto-dos-senadores-virtual.R
@@ -30,12 +30,14 @@ library(abjutils)
 
 #3. importar o nosso arquivo com o registro de todos os senadores
 # fazer o download da aba 'politicos' da planilha
-senadores_id <- fread("~/Downloads/plenario2019_SF-politicos-22jul2020.csv")
+senadores_id <- fread("~/Downloads/plenario2019_SF-politicos-22jul2020.csv", 
+                      drop = c("foto", "permalink"))
 
 #4. pegar o resultado direto via HTML
+## ALTERAR URL
 url <- "https://www.congressonacional.leg.br/materias/medidas-provisorias/-/mpv/143101/votacoes#votacao_6152"
 
-# número máximo de votantes
+# número maáximo de votantes
 number <- c(1:100)
 
 get_resultado_url <- function(x){
@@ -70,3 +72,30 @@ resultado_votacao <- resultado_votacao %>%
          voto = str_replace_all(voto, "-", "ausente")) %>%
   mutate(nome_upper = toupper(rm_accent(content))) 
   
+#5. cruzar planilhas
+joined_data <- resultado_votacao %>%
+  left_join(senadores_id, by = "nome_upper") %>%
+  arrange(desc(id))
+
+#6. informar infos da proposicao
+## ALTERAR INFORMACOES ABAIXO
+id_proposicao <- "81"
+proposicao <- "MPV986-2020"
+permalink <- "mp-sobre-recursos-nao-usados-pelo-pacote-da-lei-aldir-blanc"
+
+#7. selecionar as colunas que queremos no nosso arquivo
+votacao_final <- joined_data %>%
+  rename("nome_politico" = nome,
+         "partido" = partido,
+         "uf" = uf,
+         "id_politico" = id) %>%
+  mutate(id_proposicao = id_proposicao,
+         proposicao = proposicao,
+         permalink = permalink) %>%
+  select("id_proposicao", "proposicao", "partido", "id_politico", 
+         "nome_upper", "nome_politico", "uf", "voto", "permalink") %>% 
+  arrange(nome_upper)
+
+#8. fazer o download
+write.csv(votacao_final, paste0("votacao_final_", proposicao, Sys.Date(), ".csv")
+
