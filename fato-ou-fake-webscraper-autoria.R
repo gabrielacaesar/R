caf28949decb7e82954c8eed1b42d93faad91ecc
gabrielacaesar <gabrielacaesar55@gmail.com>
2020-08-07 16:15:50 -0300

    Update fato-ou-fake-webscraper-autoria.R

--- fato-ou-fake-webscraper-autoria.R
+++ fato-ou-fake-webscraper-autoria.R
@@ -1,46 +1,43 @@
 library(data.table)
 library(rvest)
 
-links <- fread("~/Downloads/Boatos-FOF-assinaturas-28abr2020.csv")
+links <- fread("~/Downloads/Boatos - FOF - 2020-boatos.csv")
 
 checagem <- NULL
 i <- 1
 
-while(i < 145) {
+while(i < 400) {
   tryCatch({
-  autor <- as.character(links[i]) %>%
-            read_html() %>%
-            html_node("p.content-publication-data__from") %>%
-            html_text() %>%
-            str_trim()
-  
-  titulo <- as.character(links[i]) %>%
-            read_html() %>%
-            html_nodes("h1.content-head__title") %>%
-            html_text() %>%
-            str_trim()
-            
-  data <- as.character(links[i]) %>%
-          read_html() %>%
-          html_nodes("p.content-publication-data__updated") %>%
-          html_text() %>%
-          str_split(" ") %>%
-          as.data.frame() %>%
-          `colnames<-`("data") %>%
-          .[3,] %>%
-          as.data.frame() %>%
-          `colnames<-`("data")
-  
-  checagem <- autor %>%
-            cbind(titulo, data, links[i]) %>%
-            rbind(checagem)
+autor <- as.character(links$LINK[i]) %>%
+      read_html() %>%
+      html_node("p.content-publication-data__from") %>%
+      html_text() %>%
+      str_trim()
+
+titulo <- as.character(links$LINK[i]) %>%
+      read_html() %>%
+      html_nodes("h1.content-head__title") %>%
+      html_text() %>%
+      str_trim()
+    
+data <- as.character(links$LINK[i]) %>%
+      read_html() %>%
+      html_nodes("p.content-publication-data__updated") %>%
+      html_text() %>%
+      str_split(" ") %>%
+      as.data.frame() %>%
+      `colnames<-`("data") %>%
+      .[3,] %>%
+      as.data.frame() %>%
+      `colnames<-`("data")
+    
+    checagem <- autor %>%
+      cbind(titulo, data, links$LINK[i]) %>%
+      rbind(checagem)
   }, error = function(e) return(NULL)
   )
   i <- i + 1
 }
 
-checagem_tidy <- checagem %>%
-  setnames(".", "autor") %>%
-  mutate(autor = str_remove_all(autor, "Por"))
 
-write.csv(checagem_tidy, "autoria_checagem_fof.csv")
+write.csv(checagem, paste0("autoria_checagem_", Sys.time(), ".csv"))
