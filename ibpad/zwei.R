442fec5cf6a9a085d560241d60b9593e37137fad
gabrielacaesar <gabrielacaesar55@gmail.com>
2021-02-19 19:05:48 -0300

    Add files via upload

---
+++ ibpad/zwei.R
@@ -0,0 +1,46 @@
+library(tidyverse)
+
+# funcoes gerais 
+# que podem ajudar
+# a definir o caminho
+
+list.files()
+list.dirs() # botao do STOP
+getwd()
+dir.create()
+setwd()
+Sys.Date()
+Sys.time()
+
+# capitulo 3
+
+# ler CSV
+readr::read_csv()
+
+# ler XLSX ou XLS
+readxl::read_excel()
+
+# ler com outro delimitador
+readr::read_delim()
+
+# ler a URL // arquivo 
+
+# definir encoding / colunas / tipo colunas / novo header 
+
+glimpse()
+View()
+class()
+str()
+summary()
+head()
+tail()
+unique()
+length()
+colnames()
+nrow()
+sum()
+mean()
+median()
+rename()
+
+
