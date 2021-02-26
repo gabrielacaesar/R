cb54b09f0fc43227bb5e1c125ac851dd81066710
gabrielacaesar <gabrielacaesar55@gmail.com>
2021-02-26 20:24:46 -0300

    Update script_boletins.R

--- monitor/script_boletins.R
+++ monitor/script_boletins.R
@@ -1,8 +1,6 @@
 library(tidyverse)
 library(readxl)
 
-# dados: https://github.com/gabrielacaesar/R/blob/master/monitor/dados.zip
-
 path_2020 <- "dados/2020/"
 boletins_2020 <- map_df(paste0(path_2020, list.files(path_2020, pattern = "*xls")), read.delim,
                    fileEncoding = "UTF-16LE", sep = "\t", header = T, stringsAsFactors = F)
@@ -14,14 +12,18 @@ boletins_2019 <- map_df(paste0(path_2019, list.files(path_2019, pattern = "*xls"
 b_2020 <- boletins_2020 %>%
   filter(STATUS == "Consumado") %>%
   filter(str_detect(DESDOBRAMENTO, "intervenção policial")) %>%
-  distinct(NUMERO_BOLETIM, .keep_all = TRUE) %>%
-  unite("endereco", c("LOGRADOURO", "NUMERO", "BAIRRO", "CIDADE", "UF"), sep = ", ")
+  distinct(NUMERO_BOLETIM, NOMEPESSOA, .keep_all = TRUE) %>%
+  #unite("endereco", c("LOGRADOURO", "NUMERO", "BAIRRO", "CIDADE", "UF"), sep = ", ") %>%
+  group_by(CORCUTIS) %>%
+  summarise(qt = n())
 
 b_2019 <- boletins_2019 %>%
   filter(STATUS == "Consumado") %>%
   filter(str_detect(DESDOBRAMENTO, "intervenção policial")) %>%
-  distinct(NUMERO_BOLETIM, .keep_all = TRUE) %>%
-  unite("endereco", c("LOGRADOURO", "NUMERO", "BAIRRO", "CIDADE", "UF"), sep = ", ")
+  distinct(NUMERO_BOLETIM, NOMEPESSOA, .keep_all = TRUE) %>%
+  #unite("endereco", c("LOGRADOURO", "NUMERO", "BAIRRO", "CIDADE", "UF"), sep = ", ") %>%
+  group_by(CORCUTIS) %>%
+  summarise(qt = n())
 
-#write.csv(b_2020, "b_2020.csv")
-#write.csv(b_2019, "b_2019.csv")
+# write.csv(b_2020, "b_2020.csv")
+# write.csv(b_2019, "b_2019.csv")
