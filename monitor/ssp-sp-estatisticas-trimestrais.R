e99656c882ca6f680be34bc580a7298481ae4715
gabrielacaesar <gabrielacaesar55@gmail.com>
2021-02-26 20:02:33 -0300

    Update ssp-sp-estatisticas-trimestrais.R

--- monitor/ssp-sp-estatisticas-trimestrais.R
+++ monitor/ssp-sp-estatisticas-trimestrais.R
@@ -2,33 +2,37 @@ library(tidyverse)
 library(rvest)
 
 url <- c(paste0("http://www.ssp.sp.gov.br/Estatistica/plantrim/2020-0", 
-              1:4,
-              ".htm"),
+                1:4,
+                ".htm"),
          paste0("http://www.ssp.sp.gov.br/Estatistica/plantrim/2019-0", 
-              1:4,
-              ".htm"))
+                1:4,
+                ".htm"))
 
 get_crimes <- function(i){
   url[i] %>%
-  read_html() %>%
-  html_table(fill = TRUE) %>%
-  as.data.frame() %>%
-  filter(!str_detect(X1, "Comunicado|Lei|Resolução|01Trim20")) %>%
-  filter(X2 != "") %>%
-  `colnames<-`(c("item", "crime", "capital", "grande sp",
-                "interior", "deinter_1", "deinter_2",
-                "deinter_3", "deinter_4", "deinter_5",
-                "deinter_6", "deinter_7", "deinter_8",
-                "deinter_9", "deinter_10", "estado")) %>%
-  select(-item) %>%
-  filter(!str_detect(capital, "Capital")) %>%
-  filter(!str_detect(estado, "8A") &
-         estado != "") %>%
-  mutate(boletim = url[i])
+    read_html() %>%
+    html_table(fill = TRUE) %>%
+    as.data.frame() %>%
+    filter(!str_detect(X1, "Comunicado|Lei|Resolução|01Trim20")) %>%
+    filter(X2 != "") %>%
+    `colnames<-`(c("item", "crime", "capital", "grande sp",
+                   "interior", "deinter_1", "deinter_2",
+                   "deinter_3", "deinter_4", "deinter_5",
+                   "deinter_6", "deinter_7", "deinter_8",
+                   "deinter_9", "deinter_10", "estado")) %>%
+    select(-item) %>%
+    filter(!str_detect(capital, "Capital")) %>%
+    filter(!str_detect(estado, "8A") &
+             estado != "") %>%
+    mutate(boletim = url[i])
 }
 
 todos_crimes <- map_dfr(1:length(url), get_crimes)
 
 todos_crimes <- todos_crimes %>%
   mutate(dt_boletim = basename(todos_crimes$boletim),
-         dt_boletim = str_remove_all(dt_boletim, "\\.htm"))
+         dt_boletim = str_remove_all(dt_boletim, "\\.htm")) %>%
+  separate(dt_boletim, c("ano", "mes"), sep = "-") %>%
+  filter(str_detect(crime, "mortas em confronto com a polícia militar em")) %>%
+  group_by(ano) %>%
+  summarise(qt = sum(as.numeric(estado)))
