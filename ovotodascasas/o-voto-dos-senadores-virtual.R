7f86743ddcce2e5f18177ad7be9638437eeb1b54
gabrielacaesar <gabrielacaesar55@gmail.com>
2021-03-10 16:52:03 -0300

    Update o-voto-dos-senadores-virtual.R

--- ovotodascasas/o-voto-dos-senadores-virtual.R
+++ ovotodascasas/o-voto-dos-senadores-virtual.R
@@ -61,6 +61,8 @@ joined_data <- resultado_votacao %>%
   left_join(senadores_id, by = "nome_upper") %>%
   arrange(desc(id))
 
+count(is.na(joined_data$id))
+
 #6. informar infos da proposicao
 ## ALTERAR INFORMACOES ABAIXO
 id_proposicao <- "98"
