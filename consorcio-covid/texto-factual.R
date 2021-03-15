80b853cfa1e0b418eb0f9cde544ad56b15da39d6
gabrielacaesar <gabrielacaesar55@gmail.com>
2021-03-15 17:20:48 -0300

    Update texto-factual.R

--- consorcio-covid/texto-factual.R
+++ consorcio-covid/texto-factual.R
@@ -1,4 +1,56 @@
 library(tidyverse)
+library(readxl)
+library(googledrive)
+
+# pasta para DADOS - consorcio
+dir.create(paste0("dados_consorcio_", Sys.Date()))
+setwd(paste0("dados_consorcio_", Sys.Date()))
+
+# download do XLSX - consorcio
+drive_auth(email = "gabriela.caesar.g1@gmail.com")
+drive_download(file = as_id("1qrzNw0_M5w45WlHpNT6k5I8YGaJTURyXbKIrxwbXuT0"), type = "xlsx")
+
+# leitura do XLSX - consorcio
+## ABA - BOLETIM
+arquivo <- list.files()
+boletim <- read_xlsx(arquivo, sheet = 3)
+media_movel <- read_xlsx(arquivo, sheet = 6, skip = 1)
+
+boletim_tidy <- boletim %>%
+  janitor::clean_names() %>%
+  filter(str_detect(x1, "MORTES|TESTES|DOSE")) %>%
+  .[1:11, 1:2] %>%
+  rename(tipo = x1, numero = x2) 
+
+## ABA - MEDIA MOVEL CRITERIO
+vezes_100 <- function(x){
+  return(x * 100)
+}
+
+media_movel_tidy <- media_movel %>%
+  janitor::clean_names() %>%
+  filter(dia == Sys.Date()) %>%
+  mutate_at(vars(-1), as.double) %>%
+  mutate_at(vars(starts_with("variacao")), vezes_100) %>%
+  pivot_longer(cols = -1, names_to = "tipo", values_to = "numero") %>%
+  mutate_at(vars(numero), round) %>%
+  pivot_wider(names_from = tipo, values_from = numero)
+
+# TENDENCIA - UFs
+tendencia_uf <- media_movel_tidy %>%
+  pivot_longer(-1, names_to = "tipo", values_to = "numero") %>%
+  filter(str_detect(tipo, "variacao_mortes_") & tipo != "variacao_mortes_br") %>%
+  mutate(uf = toupper(str_sub(tipo, start = -2, end = -1))) %>%
+  mutate(tendencia = case_when(numero >= 15 ~ "alta",
+            numero < 15 & numero > -15 ~ "estabilidade",
+            numero <= -15 ~ "queda"))
+
+splited_tendencia <- split(tendencia_uf, list(tendencia_uf$tendencia))
+
+splited_tendencia[1] %>%
+  as.data.frame() %>%
+  select(uf)
+
 
 # info DATA
 dia_da_semana_EN <- weekdays(Sys.Date())
@@ -27,22 +79,56 @@ mes_hoje_string <- case_when(mes_hoje == "01" ~ "janeiro",
                              mes_hoje == "12" ~ "dezembro")
 
 # info MORTES
-total_mortes <- "278.327"
-mortes_24h <- "1.111"
-media_mortes <- "1.832"
-variacao_mortes <- "+50%"
+total_mortes <- boletim_tidy$numero[1]
+mortes_24h <- boletim_tidy$numero[2]
+media_mortes <- boletim_tidy$numero[3]
+variacao_mortes <- paste0(media_movel_tidy$variacao_mortes_br, "%")
 tendencia_mortes <- case_when(parse_number(variacao_mortes) >= 15 ~ "alta",
                            parse_number(variacao_mortes) < 15 & parse_number(variacao_mortes) > -15 ~ "estabilidade",
-                           parse_number(variacao_mortes) >= -15 ~ "queda")
+                           parse_number(variacao_mortes) <= -15 ~ "queda")
 
 # info CASOS
-total_casos <- "11.483.031"
-casos_24h <- "43.781"
-media_casos <- "66.353"
-variacao_casos <- "+18%"
+total_casos <- boletim_tidy$numero[4]
+casos_24h <- boletim_tidy$numero[5]
+media_casos <- media_movel_tidy$media_movel_casos_br
+variacao_casos <- paste0(media_movel_tidy$variacao_casos_br, "%")
 tendencia_casos <- case_when(parse_number(variacao_casos) >= 15 ~ "alta",
                               parse_number(variacao_casos) < 15 & parse_number(variacao_casos) > -15 ~ "estabilidade",
-                              parse_number(variacao_casos) >= -15 ~ "queda")
+                              parse_number(variacao_casos) <= -15 ~ "queda")
+
+# info / variacao mortes - lista UFs
+variacao_PR <- paste0(media_movel_tidy$variacao_mortes_pr, "%")
+variacao_RS <- paste0(media_movel_tidy$variacao_mortes_rs, "%")
+variacao_SC <- paste0(media_movel_tidy$variacao_mortes_sc, "%")
+
+variacao_ES <- paste0(media_movel_tidy$variacao_mortes_es, "%")
+variacao_MG <- paste0(media_movel_tidy$variacao_mortes_mg, "%")
+variacao_RJ <- paste0(media_movel_tidy$variacao_mortes_rj, "%")
+variacao_SP <- paste0(media_movel_tidy$variacao_mortes_sp, "%")
+
+variacao_DF <- paste0(media_movel_tidy$variacao_mortes_df, "%")
+variacao_GO <- paste0(media_movel_tidy$variacao_mortes_go, "%")
+variacao_MS <- paste0(media_movel_tidy$variacao_mortes_ms, "%")
+variacao_MT <- paste0(media_movel_tidy$variacao_mortes_mt, "%")
+
+variacao_AC <- paste0(media_movel_tidy$variacao_mortes_ac, "%")
+variacao_AM <- paste0(media_movel_tidy$variacao_mortes_am, "%")
+variacao_AP <- paste0(media_movel_tidy$variacao_mortes_ap, "%")
+variacao_PA <- paste0(media_movel_tidy$variacao_mortes_pa, "%")
+variacao_RO <- paste0(media_movel_tidy$variacao_mortes_ro, "%")
+variacao_RR <- paste0(media_movel_tidy$variacao_mortes_rr, "%")
+variacao_TO <- paste0(media_movel_tidy$variacao_mortes_to, "%")
+
+variacao_AL <- paste0(media_movel_tidy$variacao_mortes_al, "%")
+variacao_BA <- paste0(media_movel_tidy$variacao_mortes_ba, "%")
+variacao_CE <- paste0(media_movel_tidy$variacao_mortes_ce, "%")
+variacao_MA <- paste0(media_movel_tidy$variacao_mortes_ma, "%")
+variacao_PB <- paste0(media_movel_tidy$variacao_mortes_pb, "%")
+variacao_PE <- paste0(media_movel_tidy$variacao_mortes_pe, "%")
+variacao_PI <- paste0(media_movel_tidy$variacao_mortes_pi, "%")
+variacao_RN <- paste0(media_movel_tidy$variacao_mortes_rn, "%")
+variacao_SE <- paste0(media_movel_tidy$variacao_mortes_se, "%")
+
 # PARAGRAFOS
 texto_0 <- c("Brasil registra {mortes_24h} mortes em 24 horas")
 texto_1 <- c("País contabilizou {total_casos} casos e {total_mortes} óbitos por Covid-19 desde o início da pandemia, segundo balanço do consórcio de veículos de imprensa. Casos e mortes apresentam tendência de alta.")
@@ -56,10 +142,53 @@ Média de novas mortes nos últimos 7 dias: {media_mortes} por dia (variação e
 Total de casos confirmados: {total_casos}
 Registro de casos confirmados em 24 horas: {casos_24h}
 Média de novos casos nos últimos 7 dias: {media_casos} por dia (variação em 14 dias: {variacao_casos})")
+texto_6 <- c("Sul
+PR: {variacao_PR}
+RS: {variacao_RS}
+SC: {variacao_SC}
 
+Sudeste
+ES: {variacao_ES}
+MG: {variacao_MG}
+RJ: {variacao_RJ}
+SP: {variacao_SP}
+
+Centro-Oeste
+DF: {variacao_DF}
+GO: {variacao_GO}
+MS: {variacao_MS}
+MT: {variacao_MT}
+
+Norte
+AC: {variacao_AC}
+AM: {variacao_AM}
+AP: {variacao_AP}
+PA: {variacao_PA}
+RO: {variacao_RO}
+RR: {variacao_RR}
+TO: {variacao_TO}
+
+Nordeste
+AL: {variacao_AL}
+BA: {variacao_BA}
+CE: {variacao_CE}
+MA: {variacao_MA}
+PB: {variacao_PB}
+PE: {variacao_PE}
+PI: {variacao_PI}
+RN: {variacao_RN}
+SE: {variacao_SE}")
+texto_7 <- c("Estados
+Subindo ({alta_n_uf} estados): {alta_lista_uf}
+Em estabilidade ({estabilidade_n_uf} estado): {estabilidade_lista_uf}
+Em queda ({queda_n_uf} estados): {queda_lista_uf}")
+
+# GLUUUUUUE
 str_glue(texto_0)
 str_glue(texto_1)
 str_glue(texto_2)
 str_glue(texto_3)
 str_glue(texto_4)
 str_glue(texto_5)
+str_glue(texto_6)
+str_glue(texto_7)
