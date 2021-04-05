5ef1438f29bf202b999760ad7225c60b2dd424ed
gabrielacaesar <gabrielacaesar55@gmail.com>
2021-04-05 15:55:47 -0300

    Update analise-boletim-cor-raca.R

--- monitor/analise-boletim-cor-raca.R
+++ monitor/analise-boletim-cor-raca.R
@@ -3,41 +3,12 @@ library(tidyverse)
 
 dados <- read_xlsx("dados/outros/MDIP_2013_Jan 2021.xlsx")
 
-###### POLICIA MILITAR
-b_2020_SERVICO_PM <- dados %>%
+pessoas_mortas <- dados %>%
   janitor::clean_names() %>%
-  filter(ano_bo == 2020, 
-         ano == 2020,
-         coorporacao == "PM") %>%
-  filter(str_detect(situacao, "Serviço|serviço")) %>%
-  group_by(cor) %>%
+  filter(ano_bo %in% c(2019, 2020), 
+         ano %in% c(2019, 2020),
+         coorporacao %in% c("PM", "PC")) %>%
+  filter(str_detect(situacao, "Serviço|serviço|Folga|folga")) %>%
+  filter(municipio_circ == "S.PAULO") %>%
+  group_by(coorporacao, ano_bo) %>%
   summarise(qt = n())
-
-b_2020_FOLGA_PM <- dados %>%
-  janitor::clean_names() %>%
-  filter(ano_bo == 2020, 
-         ano == 2020,
-         coorporacao == "PM") %>%
-  filter(str_detect(situacao, "Folga|folga")) %>%
-  group_by(cor) %>%
-  summarise(qt = n())
-
-###### POLICIA CIVIL
-b_2020_SERVICO_PC <- dados %>%
-  janitor::clean_names() %>%
-  filter(ano_bo == 2020, 
-         ano == 2020,
-         coorporacao == "PC") %>%
-  filter(str_detect(situacao, "Serviço|serviço")) %>%
-  group_by(cor) %>%
-  summarise(qt = n())
-
-b_2020_FOLGA_PC <- dados %>%
-  janitor::clean_names() %>%
-  filter(ano_bo == 2020, 
-         ano == 2020,
-         coorporacao == "PC") %>%
-  filter(str_detect(situacao, "Folga|folga")) %>%
-  group_by(cor) %>%
-  summarise(qt = n())
-
