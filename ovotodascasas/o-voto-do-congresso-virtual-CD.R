207df7edaee4a7640fe9b1d15baf4207cbb53c3a
gabrielacaesar <gabrielacaesar55@gmail.com>
2021-04-06 21:54:34 -0300

    Update o-voto-do-congresso-virtual-CD.R

--- ovotodascasas/o-voto-do-congresso-virtual-CD.R
+++ ovotodascasas/o-voto-do-congresso-virtual-CD.R
@@ -27,6 +27,7 @@ library(foreign)
 library(rvest)
 library(data.table)
 library(abjutils)
+library(readxl)
 
 #3. importar o nosso arquivo com o registro de todos os deputados
 # fazer o download da aba 'politicos' da planilha
@@ -41,7 +42,7 @@ deputados_id <- fread("~/Downloads/plenario2019_CD - politicos.csv")
 # ATENÇÃO: APENAS caso não importe o resultado via arquivo (etapa 4-A)
 # caminho para achar a URL: Atividade legislativa > Agenda > (selecionar o dia) > (selecionar a sessão) > Votação > (selecionar a votação)
 # indicar NOVA URL abaixo
-url <- "https://www.camara.leg.br/internet/votacao/mostraVotacao.asp?ideVotacao=9408&numLegislatura=56&codCasa=4&numSessaoLegislativa=3&indTipoSessaoLegislativa=O&numSessao=6&indTipoSessao=E&tipo=partido"
+url <- "https://www.camara.leg.br/internet/votacao/mostraVotacao.asp?ideVotacao=9442&numLegislatura=56&codCasa=1&numSessaoLegislativa=3&indTipoSessaoLegislativa=O&numSessao=32&indTipoSessao=E&tipo=partido"
 
 resultado_url <- url %>%
   read_html() %>%
@@ -58,9 +59,42 @@ resultado_url <- url %>%
          uf = UF,
          voto = Voto) %>%
   mutate(nome_upper = toupper(abjutils::rm_accent(nome)),
-         voto = tolower(abjutils::rm_accent(voto)))
+         voto = tolower(abjutils::rm_accent(voto))) %>%
+  mutate(nome_upper = str_replace_all(nome_upper, "BOZZELLA", "JUNIOR BOZZELLA"),
+         nome_upper = str_replace_all(nome_upper, "GLAUSTIN DA FOKUS", "GLAUSTIN FOKUS"),
+         nome_upper = str_replace_all(nome_upper, "JOSE AIRTON FELIX CIRILO", "JOSE AIRTON CIRILO"),
+         nome_upper = str_replace_all(nome_upper, "JUNIO AMARAL", "CABO JUNIO AMARAL"),
+         nome_upper = str_replace_all(nome_upper, "OTTACI NASCIMENTO", "OTACI NASCIMENTO"),
+         nome_upper = str_replace_all(nome_upper, "PASTOR GIL", "PASTOR GILDENEMYR"),
+         nome_upper = str_replace_all(nome_upper, "ROMAN", "EVANDRO ROMAN"),
+         nome_upper = str_replace_all(nome_upper, "VITOR HUGO", "MAJOR VITOR HUGO"))
 
 # url doesn't contain absents deputies
+# link - exercico https://www.camara.leg.br/internet/deputado/deputado.xls
+dep_exercicio <- read_xls("~/Downloads/deputado.xls")
 
+merged <- dep_exercicio %>%
+  janitor::clean_names() %>%
+  mutate(nome_upper = abjutils::rm_accent(toupper(nome_parlamentar))) %>%
+  mutate(nome_upper = str_replace_all(nome_upper, "BOZZELLA", "JUNIOR BOZZELLA"),
+         nome_upper = str_replace_all(nome_upper, "GLAUSTIN DA FOKUS", "GLAUSTIN FOKUS"),
+         nome_upper = str_replace_all(nome_upper, "JOSE AIRTON FELIX CIRILO", "JOSE AIRTON CIRILO"),
+         nome_upper = str_replace_all(nome_upper, "JUNIO AMARAL", "CABO JUNIO AMARAL"),
+         nome_upper = str_replace_all(nome_upper, "OTTACI NASCIMENTO", "OTACI NASCIMENTO"),
+         nome_upper = str_replace_all(nome_upper, "PASTOR GIL", "PASTOR GILDENEMYR"),
+         nome_upper = str_replace_all(nome_upper, "ROMAN", "EVANDRO ROMAN"),
+         nome_upper = str_replace_all(nome_upper, "VITOR HUGO", "MAJOR VITOR HUGO")) %>%
+  select(nome_upper, partido, uf) %>%
+  left_join(resultado_url, by = "nome_upper") %>%  
+  left_join(deputados_id, by = "nome_upper") %>%
+  rename(id_politico = id,
+         nome_politico = `nome.y`) %>%
+  mutate(voto = str_replace_na(voto, "ausente"),
+         voto = str_replace_all(voto, "art. 17", "naovotou"),
+         id_proposicao = "71",
+         proposicao = "PL948-2021",
+         permalink = "compra-de-vacinas-da-covid-19-por-empresas") %>%
+  select(id_proposicao, proposicao, partido, id_politico, 
+         nome_upper, nome_politico, uf, voto, permalink)
 
-
+write.csv(merged, "merged_2.csv")
